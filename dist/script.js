cd4038492d58521f20311e0e1a5531fef10091ff
Olga <olga@cottondroid.com>
2024-02-10 12:38:02 +0000

    v0.1

---
+++ dist/script.js
@@ -0,0 +1,171 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var selectedWord = "";
+// Function to fetch words from a file
+function fetchWordsFromFile() {
+    return __awaiter(this, void 0, void 0, function () {
+        var response, text, wordlist, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 3, , 4]);
+                    return [4 /*yield*/, fetch('/assets/wordlist')];
+                case 1:
+                    response = _a.sent();
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch words from file');
+                    }
+                    return [4 /*yield*/, response.text()];
+                case 2:
+                    text = _a.sent();
+                    wordlist = text.split('\n').map(function (word) { return word.trim(); });
+                    console.log(wordlist);
+                    return [2 /*return*/, wordlist];
+                case 3:
+                    error_1 = _a.sent();
+                    console.error('Error fetching words:', error_1);
+                    return [2 /*return*/, []]; // Return an empty array in case of error
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+// Function to randomly select a word from the list
+function selectRandomWord(words) {
+    var randomIndex = Math.floor(Math.random() * words.length); // Generate a random index
+    return words[randomIndex]; // Return the word at the randomly selected index
+}
+// Function to speak the word using Text-to-Speech
+function speakWord() {
+    var utterance = new SpeechSynthesisUtterance(selectedWord);
+    utterance.lang = 'en-GB';
+    speechSynthesis.speak(utterance);
+}
+function revealWord() {
+    var revealedWord = document.getElementById('revealedWord');
+    document.getElementById('inputContainer').style.display = 'none';
+    document.getElementById('revealedWordContainer').style.display = 'block';
+    revealedWord.textContent = selectedWord;
+}
+function displayFailIcon() {
+    var failIcon = document.createElement("span");
+    failIcon.className = "icon";
+    failIcon.textContent = "❌";
+    failIcon.style.color = "red";
+    document.getElementById('inputContainer').appendChild(failIcon);
+}
+function displaySuccessIcon() {
+    var matchIcon = document.createElement("span");
+    matchIcon.className = "icon";
+    matchIcon.textContent = "✓";
+    matchIcon.style.color = "green";
+    document.getElementById('revealedWord').appendChild(matchIcon);
+}
+function initPlayButton() {
+    var playButton = document.getElementById('playButton');
+    playButton.addEventListener('click', function () { return speakWord(); });
+}
+function initSubmitButton() {
+    var checkButton = document.getElementById('checkButton');
+    checkButton.addEventListener('click', function () {
+        var inputWord = document.getElementById('wordInput').value.trim();
+        var success = inputWord.toLowerCase() === selectedWord.toLowerCase();
+        if (success) {
+            revealWord();
+            displaySuccessIcon();
+        }
+        else {
+            displayFailIcon();
+        }
+    });
+}
+function initRevealButton() {
+    var revealButton = document.getElementById('revealButton');
+    revealButton.addEventListener('click', function () {
+        revealWord();
+    });
+}
+// Function to refresh and select a new random word
+function refreshWord(words) {
+    var _a;
+    // Select a new random word
+    selectedWord = selectRandomWord(words);
+    // Reset all fields
+    document.getElementById('wordInput').value = ''; // Clear input field
+    document.getElementById('inputContainer').style.display = 'block'; // Show input container;
+    document.getElementById('revealedWordContainer').style.display = 'none'; // Hide revealed word container
+    document.getElementById('revealedWord').textContent = ''; // Clear revealed word
+    (_a = document.querySelector('#revealedWord .icon')) === null || _a === void 0 ? void 0 : _a.remove(); // Remove success icon
+    var failIcons = document.querySelectorAll('#inputContainer .icon');
+    failIcons.forEach(function (icon) {
+        icon.remove();
+    });
+}
+// Function to initialize the refresh button
+function initRefreshButton(words) {
+    var refreshButton = document.getElementById('refreshButton');
+    refreshButton.addEventListener('click', function () {
+        refreshWord(words);
+    });
+}
+function init() {
+    return __awaiter(this, void 0, void 0, function () {
+        var words, error_2;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    return [4 /*yield*/, fetchWordsFromFile()];
+                case 1:
+                    words = _a.sent();
+                    selectedWord = selectRandomWord(words);
+                    initPlayButton();
+                    initSubmitButton();
+                    initRevealButton();
+                    initRefreshButton(words);
+                    return [3 /*break*/, 3];
+                case 2:
+                    error_2 = _a.sent();
+                    console.error('Error creating and checking word:', error_2);
+                    return [3 /*break*/, 3];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
+}
+init();
---
+++ src/script.ts
@@ -0,0 +1,125 @@
+var selectedWord = ""
+
+// Function to fetch words from a file
+async function fetchWordsFromFile(): Promise<string[]> {
+  try {
+    const response = await fetch('/assets/wordlist'); // Fetch the file from the server
+    if (!response.ok) {
+      throw new Error('Failed to fetch words from file');
+    }
+    const text = await response.text(); // Get the file content as text
+    const wordlist = text.split('\n').map(word => word.trim()); // Split content by new lines and trim whitespace
+    console.log(wordlist)
+    return wordlist
+  } catch (error) {
+    console.error('Error fetching words:', error);
+    return []; // Return an empty array in case of error
+  }
+}
+
+// Function to randomly select a word from the list
+function selectRandomWord(words: string[]): string {
+  const randomIndex = Math.floor(Math.random() * words.length); // Generate a random index
+  return words[randomIndex]; // Return the word at the randomly selected index
+}
+
+// Function to speak the word using Text-to-Speech
+function speakWord() {
+  const utterance = new SpeechSynthesisUtterance(selectedWord);
+  utterance.lang = 'en-GB';
+  speechSynthesis.speak(utterance);
+}
+
+function revealWord() {
+  const revealedWord = document.getElementById('revealedWord')!;
+  document.getElementById('inputContainer')!.style.display = 'none';
+  document.getElementById('revealedWordContainer')!.style.display = 'block';
+  revealedWord.textContent = selectedWord;
+}
+
+function displayFailIcon() {
+  const failIcon = document.createElement("span");
+  failIcon.className = "icon"
+  failIcon.textContent = "❌";
+  failIcon.style.color = "red";
+  document.getElementById('inputContainer')!.appendChild(failIcon);
+}
+
+function displaySuccessIcon() {
+  const matchIcon = document.createElement("span");
+  matchIcon.className = "icon"
+  matchIcon.textContent = "✓";
+  matchIcon.style.color = "green";
+  document.getElementById('revealedWord')!.appendChild(matchIcon);
+}
+
+
+function initPlayButton() {
+  const playButton = document.getElementById('playButton');
+  playButton!.addEventListener('click', () => speakWord());
+}
+
+function initSubmitButton() {
+  const checkButton = document.getElementById('checkButton')!;
+  checkButton.addEventListener('click', () => {
+    const inputWord = (document.getElementById('wordInput') as HTMLInputElement).value.trim();
+    const success = inputWord.toLowerCase() === selectedWord.toLowerCase();
+    if (success) {
+      revealWord()
+      displaySuccessIcon()
+    } else {
+      displayFailIcon()
+    }
+  });
+}
+
+function initRevealButton() {
+  const revealButton = document.getElementById('revealButton');
+  revealButton!.addEventListener('click', () => {
+    revealWord();
+  });
+
+}
+
+// Function to refresh and select a new random word
+function refreshWord(words: string[]) {
+  // Select a new random word
+  selectedWord = selectRandomWord(words);
+
+  // Reset all fields
+  (document.getElementById('wordInput') as HTMLInputElement).value = ''; // Clear input field
+  document.getElementById('inputContainer')!.style.display = 'block'; // Show input container;
+  document.getElementById('revealedWordContainer')!.style.display = 'none'; // Hide revealed word container
+  document.getElementById('revealedWord')!.textContent = ''; // Clear revealed word
+  document.querySelector('#revealedWord .icon')?.remove(); // Remove success icon
+  const failIcons = document.querySelectorAll('#inputContainer .icon');
+  failIcons.forEach(icon => {
+    icon.remove();
+  });
+}
+
+// Function to initialize the refresh button
+function initRefreshButton(words: string[]): void {
+  const refreshButton = document.getElementById('refreshButton');
+  refreshButton!.addEventListener('click', () => {
+    refreshWord(words);
+  });
+}
+
+
+async function init(): Promise<void> {
+  try {
+    const words = await fetchWordsFromFile();
+    selectedWord = selectRandomWord(words);
+
+    initPlayButton();
+    initSubmitButton();
+    initRevealButton();
+    initRefreshButton(words);
+
+  } catch (error) {
+    console.error('Error creating and checking word:', error);
+  }
+}
+
+init();
