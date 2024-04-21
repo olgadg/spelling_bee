80d568f57ed5a62681c2028e098ecf5dc55d92e1
Olga <olga@cottondroid.com>
2024-04-21 21:40:06 +0100

    prettify things

--- src/script.ts
+++ src/script.ts
@@ -1,28 +1,31 @@
-var selectedWord = ""
-var savedWords :WordCountMap
+var selectedWord = "";
+var savedWords: WordCountMap;
 
 type WordCountMap = { [word: string]: number };
 
 // Function to fetch words from a file
 async function fetchWordsFromFile(): Promise<string[]> {
   try {
-    const response = await fetch('/assets/wordlist'); // Fetch the file from the server
+    const response = await fetch("/assets/wordlist"); // Fetch the file from the server
     if (!response.ok) {
-      throw new Error('Failed to fetch words from file');
+      throw new Error("Failed to fetch words from file");
     }
     const text = await response.text(); // Get the file content as text
-    const wordlist = text.split('\n').map(word => word.trim()); // Split content by new lines and trim whitespace
+    const wordlist = text.split("\n").map((word) => word.trim()); // Split content by new lines and trim whitespace
     console.log("wordlist", wordlist);
-    return wordlist
+    return wordlist;
   } catch (error) {
-    console.error('Error fetching words:', error);
+    console.error("Error fetching words:", error);
     return []; // Return an empty array in case of error
   }
 }
 
 // Function to read the cookie and parse the word count map
 function readWordCountFromCookie(): WordCountMap {
-  const cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)wordCount\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  const cookieValue = document.cookie.replace(
+    /(?:(?:^|.*;\s*)wordCount\s*\=\s*([^;]*).*$)|^.*$/,
+    "$1",
+  );
   return cookieValue ? JSON.parse(decodeURIComponent(cookieValue)) : {};
 }
 
@@ -40,7 +43,10 @@ function selectWeightedRandomWord(): string {
   const maxCount = Math.max(...wordCountArray.map(([, count]) => count));
 
   // Calculate the total "inverse" count, where each word's inverse count is (maxCount - count + 1)
-  const totalInverseCount = wordCountArray.reduce((acc, [, count]) => acc + (maxCount - count + 1), 0);
+  const totalInverseCount = wordCountArray.reduce(
+    (acc, [, count]) => acc + (maxCount - count + 1),
+    0,
+  );
 
   // Generate a random number between 0 and total inverse count
   const randomNumber = Math.random() * totalInverseCount;
@@ -53,71 +59,69 @@ function selectWeightedRandomWord(): string {
       return word; // Return the word when the cumulative inverse count exceeds the random number
     }
   }
-  return ""
+  return "";
 }
 
 // Function to speak the word using Text-to-Speech
 function speakWord() {
-  console.log(selectedWord)
+  console.log(selectedWord);
   const utterance = new SpeechSynthesisUtterance(selectedWord);
-  utterance.lang = 'en-GB';
+  utterance.lang = "en-GB";
   speechSynthesis.speak(utterance);
 }
 
 function revealWord() {
-  const revealedWord = document.getElementById('revealedWord')!;
-  document.getElementById('inputContainer')!.style.display = 'none';
-  document.getElementById('revealedWordContainer')!.style.display = 'flex';
+  const revealedWord = document.getElementById("revealedWord")!;
+  document.getElementById("inputContainer")!.style.display = "none";
+  document.getElementById("revealedWordContainer")!.style.display = "flex";
   revealedWord.textContent = selectedWord;
 }
 
 function displaySuccessIcon() {
   const matchIcon = document.createElement("span");
-  matchIcon.className = "icon"
+  matchIcon.className = "icon";
   matchIcon.textContent = "✓";
   matchIcon.style.color = "green";
-  document.getElementById('revealedWord')!.appendChild(matchIcon);
+  document.getElementById("revealedWord")!.appendChild(matchIcon);
 }
 
 function displayFailIcon() {
   const failIcon = document.createElement("span");
-  failIcon.className = "icon"
+  failIcon.className = "icon";
   failIcon.textContent = "❌";
   failIcon.style.color = "red";
-  document.getElementById('resultIcon')!.appendChild(failIcon);
+  document.getElementById("resultIcon")!.appendChild(failIcon);
 }
 
 function initPlayButton() {
-  const playButton = document.getElementById('playButton');
-  playButton!.addEventListener('click', () => speakWord());
+  const playButton = document.getElementById("playButton");
+  playButton!.addEventListener("click", () => speakWord());
 }
 
 function initSubmitButton() {
-  
-  document.getElementById('revealedWordContainer')!.style.display = 'none';
-  const wordInput = (document.getElementById('wordInput') as HTMLInputElement);
-  wordInput.type = 'text';
-  const checkButton = document.getElementById('checkButton')!;
-  checkButton.addEventListener('click', () => {
+  document.getElementById("revealedWordContainer")!.style.display = "none";
+  const wordInput = document.getElementById("wordInput") as HTMLInputElement;
+  wordInput.type = "text";
+  const checkButton = document.getElementById("checkButton")!;
+  checkButton.addEventListener("click", () => {
     const inputWord = wordInput.value.trim();
     const success = inputWord.toLowerCase() === selectedWord.toLowerCase();
     if (success) {
       savedWords[inputWord] = (savedWords[inputWord] || 0) + 5;
-      saveWordCountToCookie(savedWords)
-      revealWord()
-      displaySuccessIcon()
+      saveWordCountToCookie(savedWords);
+      revealWord();
+      displaySuccessIcon();
     } else {
-      displayFailIcon()
+      displayFailIcon();
     }
   });
 }
 
 function initRevealButton() {
-  const revealButton = document.getElementById('revealButton');
-  revealButton!.addEventListener('click', () => {
+  const revealButton = document.getElementById("revealButton");
+  revealButton!.addEventListener("click", () => {
     revealWord();
   });
-
 }
 
 // Function to refresh and select a new random word
@@ -126,38 +130,37 @@ function refreshWord() {
   selectedWord = selectWeightedRandomWord();
 
   // Reset all fields
-  (document.getElementById('wordInput') as HTMLInputElement).value = ''; // Clear input field
-  document.getElementById('inputContainer')!.style.display = 'flex'; // Show input container;
-  document.getElementById('revealedWordContainer')!.style.display = 'none'; // Hide revealed word container
-  document.getElementById('revealedWord')!.textContent = ''; // Clear revealed word
-  document.querySelector('#revealedWord .icon')?.remove(); // Remove success icon
-  const failIcons = document.querySelectorAll('#resultIcon .icon');
-  failIcons.forEach(icon => {
+  (document.getElementById("wordInput") as HTMLInputElement).value = ""; // Clear input field
+  document.getElementById("inputContainer")!.style.display = "flex"; // Show input container;
+  document.getElementById("revealedWordContainer")!.style.display = "none"; // Hide revealed word container
+  document.getElementById("revealedWord")!.textContent = ""; // Clear revealed word
+  document.querySelector("#revealedWord .icon")?.remove(); // Remove success icon
+  const failIcons = document.querySelectorAll("#resultIcon .icon");
+  failIcons.forEach((icon) => {
     icon.remove();
   });
 }
 
 // Function to initialize the refresh button
 function initRefreshButton(): void {
-  const refreshButton = document.getElementById('refreshButton');
-  if (!refreshButton) return
+  const refreshButton = document.getElementById("refreshButton");
+  if (!refreshButton) return;
 
-  refreshButton.addEventListener('click', () => {
+  refreshButton.addEventListener("click", () => {
     refreshWord();
     speakWord();
   });
 }
 
-
 async function init(): Promise<void> {
   try {
     const words = await fetchWordsFromFile();
     savedWords = readWordCountFromCookie();
-    console.log("savedWords", savedWords)
+    console.log("savedWords", savedWords);
 
-    if(Object.keys(savedWords).length == 0) {
-      words.forEach(word => savedWords[word] = 0)
-      saveWordCountToCookie(savedWords)
+    if (Object.keys(savedWords).length == 0) {
+      words.forEach((word) => (savedWords[word] = 0));
+      saveWordCountToCookie(savedWords);
     }
 
     selectedWord = selectWeightedRandomWord();
@@ -166,9 +169,8 @@ async function init(): Promise<void> {
     initSubmitButton();
     initRevealButton();
     initRefreshButton();
-
   } catch (error) {
-    console.error('Error creating and checking word:', error);
+    console.error("Error creating and checking word:", error);
   }
 }
 
